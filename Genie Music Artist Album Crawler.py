c1222d3e322c50b6b93b1f735d4a6988fefe4fcc
IZH318 <99892351+IZH318@users.noreply.github.com>
2024-07-13 06:58:24 +0900

    # 2024-07-13

--- Genie Music Artist Album Crawler.py
+++ Genie Music Artist Album Crawler.py
@@ -1,39 +1,83 @@
-# Selenium에서 필요한 모듈
-from selenium import webdriver  # 웹 드라이버 관련 모듈
-from selenium.webdriver.chrome.service import Service  # Chrome 서비스 관련 모듈
-from selenium.webdriver.chrome.options import Options  # Chrome 옵션 관련 모듈
-from selenium.webdriver.common.by import By  # 웹 요소를 검색하기 위한 방법(By) 관련 모듈
+import os  # 운영 체제 관련 기능을 사용하기 위한 모듈
+import re  # 정규 표현식을 사용하기 위한 모듈
+import time  # 시간 관련 기능을 사용하기 위한 모듈
+import threading  # 병렬 처리를 위한 모듈
+
+# Selenium 관련 모듈
+from selenium import webdriver  # 웹 브라우저를 제어하기 위한 모듈
+from selenium.webdriver.chrome.service import Service  # Chrome 드라이버 서비스 설정을 위한 모듈
+from selenium.webdriver.chrome.options import Options  # Chrome 브라우저 옵션 설정을 위한 모듈
+from selenium.webdriver.common.by import By  # 웹 요소를 검색하기 위한 모듈
 from selenium.webdriver.support.ui import WebDriverWait  # 웹 페이지가 로드될 때까지 대기하기 위한 모듈
-from selenium.webdriver.support import expected_conditions as EC  # 조건을 기다리기 위한 모듈
-from selenium.webdriver.common.action_chains import ActionChains  # 마우스 액션을 수행하기 위한 모듈
+from selenium.webdriver.support import expected_conditions as EC  # 조건이 충족될 때까지 대기하기 위한 모듈
+from selenium.webdriver.common.action_chains import ActionChains  # 웹 요소에 대한 마우스 및 키보드 동작을 시뮬레이션하기 위한 모듈
 
-# BeautifulSoup를 사용하여 HTML 파싱을 위한 모듈
-from bs4 import BeautifulSoup
-
-# 정규 표현식 및 시간 관련 모듈
-import re
-import time
-
-# GUI 요소를 생성하기 위한 모듈
-import tkinter as tk
-from tkinter import Menu, Button, Text, Scrollbar, BooleanVar, messagebox, filedialog
-
-# 다중 스레드를 위한 threading 모듈
-import threading
+# BeautifulSoup 및 GUI 관련 모듈
+from bs4 import BeautifulSoup  # HTML 및 XML 문서를 구문 분석하기 위한 모듈
+import tkinter as tk  # GUI를 만들기 위한 모듈
+from tkinter import Menu, Button, Text, Scrollbar, BooleanVar, messagebox, filedialog  # GUI 구성 요소를 사용하기 위한 모듈
 
 
 
-def split_desc(desc):
-    parts = desc.split('|')
-    release_date = parts[0].strip() if len(parts) > 0 else 'N/A'
-    track_count = parts[2].strip() if len(parts) > 2 else 'N/A'
-    return release_date, track_count
+def find_chromedriver():
+    # 현재 스크립트가 위치한 디렉터리 경로
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # 현재 디렉터리 및 하위 디렉터리에서 chromedriver.exe 파일을 찾기
+    for root, dirs, files in os.walk(current_dir):
+        if 'chromedriver.exe' in files:
+            return os.path.join(root, 'chromedriver.exe')
+    
+    # 찾지 못한 경우 None 반환
+    return None
+
+
+
+def start_fetching(url_entry, text_area, delete_url_after_download):
+    url = url_entry.get()
+    if url.startswith('https://www.genie.co.kr/detail/artistAlbum?xxnm='):
+        # 앨범 정보 가져오기 버튼 비활성화
+        url_entry.config(state=tk.DISABLED)
+        fetch_button.config(state=tk.DISABLED)
+        button_clear.config(state=tk.DISABLED)
+
+        # 지연 시간 설정 (초 단위)
+        delay = 5
+        
+        text_area.insert(tk.END, f"\n [알림] {delay}초 후 작업이 진행 됩니다.\n\n", 'info')
+        text_area.tag_configure('info', foreground='black', background='yellow', font=('Arial', 12, 'bold'))
+
+        text_area.insert(tk.END, f"\n\n")
+        
+        text_area.insert(tk.END, "\n [경고] 자동화 작업이 진행되는 웹 브라우저를 임의로 작업하지 마십시오.\n\n", 'warning')
+        text_area.tag_configure('warning', foreground='white', background='red', font=('Arial', 12, 'bold'))
+
+        text_area.insert(tk.END, f"\n")
+
+        
+        text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
+        
+        thread = threading.Thread(target=fetch_album_info, args=(url, text_area, delete_url_after_download, url_entry, delay))
+        thread.start()
+    else:
+        text_area.insert(tk.END, "\n[안내] 잘못된 URL입니다. 올바른 URL을 입력하십시오.\n\n")
+        text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
 
 
 
 def fetch_album_info(url, text_area, delete_url_after_download, url_entry, delay):
-    chrome_driver_path = r'./ChromeDriver/chromedriver.exe'  # ChromeDriver 상대 경로
-
+    chrome_driver_path = find_chromedriver()
+    
+    if not chrome_driver_path:
+        text_area.insert(tk.END, "\n[경고] chromedriver.exe를 찾을 수 없습니다.\n")
+        
+        url_entry.config(state=tk.NORMAL)
+        fetch_button.config(state=tk.NORMAL)
+        button_clear.config(state=tk.NORMAL)
+            
+        text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
+        return
+    
     options = Options()
     options.add_argument('--start-maximized')
     options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
@@ -63,7 +107,7 @@ def fetch_album_info(url, text_area, delete_url_after_download, url_entry, delay
                 albums = soup.select('li.list-album')
                 
                 if not albums:
-                    text_area.insert(tk.END, "[안내] 페이지에서 앨범을 찾을 수 없습니다.\n")
+                    text_area.insert(tk.END, "\n[안내] 페이지에서 앨범을 찾을 수 없습니다.")
                     text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
                     break
 
@@ -95,22 +139,22 @@ def fetch_album_info(url, text_area, delete_url_after_download, url_entry, delay
                     else:
                         break
                 except Exception as e:
-                    text_area.insert(tk.END, f"[경고] 다음 페이지로 이동하는 버튼을 찾을 수 없거나 다른 문제가 발생하였습니다.: {e}\n")
+                    text_area.insert(tk.END, f"\n[경고] 다음 페이지로 이동하는 버튼을 찾을 수 없거나 다른 문제가 발생하였습니다.: {e}\n")
                     text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
                     break
 
             except Exception as e:
-                text_area.insert(tk.END, f"[알림] 예외 발생: {e}\n\n\n")
+                text_area.insert(tk.END, f"\n[알림] 예외 발생: {e}\n\n\n")
                 try:
                     alert = driver.switch_to.alert
                     alert_text = alert.text
                     alert.accept()
-                    text_area.insert(tk.END, f"[안내] 알림이 수락되었습니다: {alert_text}\n\n")
+                    text_area.insert(tk.END, f"\n[안내] 알림이 수락되었습니다: {alert_text}\n\n")
                     if "마지막 페이지입니다" in alert_text:
                         text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
                         break
                 except Exception as alert_exception:
-                    text_area.insert(tk.END, f"[안내] Failed to handle alert: {alert_exception}\n\n\n")
+                    text_area.insert(tk.END, f"\n[안내] Failed to handle alert: {alert_exception}\n\n\n")
                     text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
                 break
 
@@ -118,9 +162,10 @@ def fetch_album_info(url, text_area, delete_url_after_download, url_entry, delay
             # text_area 초기화
             text_area.delete(1.0, tk.END)
             
-            text_area.insert(tk.END, f"\n[알림] 모든 작업이 정상적으로 처리되었습니다.\n\n\n\n")
-
-            text_area.insert(tk.END, f"===== {artist_name} Album Information =====\n\n")
+            text_area.insert(tk.END, f"\n [알림] 모든 작업이 정상적으로 처리되었습니다.\n\n", 'info')
+            text_area.tag_configure('info', foreground='black', background='yellow', font=('Arial', 12, 'bold'))
+            
+            text_area.insert(tk.END, f"\n\n\n===== [{artist_name}] Album Information =====\n\n")
             
             for album_info in album_info_list:
                 text_area.insert(tk.END, '-' * 40 + '\n')
@@ -132,7 +177,7 @@ def fetch_album_info(url, text_area, delete_url_after_download, url_entry, delay
                 text_area.insert(tk.END, '-' * 40 + '\n\n')
 
         else:
-            text_area.insert(tk.END, "[안내] 앨범 정보를 찾을 수 없습니다.\n\n")
+            text_area.insert(tk.END, "\n[안내] 앨범 정보를 찾을 수 없습니다.\n\n")
             text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
     
     finally:
@@ -148,29 +193,13 @@ def fetch_album_info(url, text_area, delete_url_after_download, url_entry, delay
 
 
 
-def start_fetching(url_entry, text_area, delete_url_after_download):
-    url = url_entry.get()
-    if url.startswith('https://www.genie.co.kr/detail/artistAlbum?xxnm='):
-        
-        # 앨범 정보 가져오기 버튼 비활성화
-        url_entry.config(state=tk.DISABLED)
-        fetch_button.config(state=tk.DISABLED)
-        button_clear.config(state=tk.DISABLED)
+def split_desc(desc):
+    parts = desc.split('|')
+    release_date = parts[0].strip() if len(parts) > 0 else 'N/A'
+    track_count = parts[2].strip() if len(parts) > 2 else 'N/A'
+    return release_date, track_count
 
-        # 지연 시간 설정 (초 단위)
-        delay = 5
-        
-        text_area.insert(tk.END, f"\n[알림] {delay}초 후 작업이 진행 됩니다.\n\n")
-        text_area.insert(tk.END, f"[경고] 작업이 진행되는 웹 브라우저를 임의로 작업하지 마십시오.\n\n")
-        # text_area.insert(tk.END, "[알림] 아래에 표기 된 내용은 오류 코드가 아닙니다.\n\n\n\n")
-        
-        text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
-        
-        thread = threading.Thread(target=fetch_album_info, args=(url, text_area, delete_url_after_download, url_entry, delay))
-        thread.start()
-    else:
-        text_area.insert(tk.END, "[안내] 잘못된 URL입니다. 올바른 URL을 입력하십시오.\n\n")
-        text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
+
 
 def save_to_file():
     file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("텍스트 문서", "*.txt")])
@@ -182,6 +211,8 @@ def save_to_file():
         except Exception as e:
             messagebox.showerror("알림", f"파일을 저장하는 중 오류가 발생했습니다: {e}")
 
+
+
 def exit_program():
     root.destroy()
 
@@ -191,20 +222,18 @@ def show_help():
     help_text = """[안내]
 음원 스트리밍 사이트 지니뮤직(Genie Music)에 등록된 가수의 모든 앨범 정보를 한 번에 크롤링(Crawling) 합니다.
 
-
-
 [체크 박스]
 작업 후 아티스트 앨범 URL 값 초기화:
 ㄴ 비 활성화(기본값): 입력 값 유지
 ㄴ 활성화: 모든 작업이 끝나면 입력 값 삭제
-
-
 
 [버튼]
 앨범 정보 가져오기: 지니뮤직 아티스트 앨범 페이지에 있는 모든 정보를 가지고 옴
 log Clear: 화면 하단 Log 값 모두 삭제
 """
     messagebox.showinfo("도움말(Help)", help_text)
+
+
 
 def show_program_about():
     show_program_about_text = """Genie Music Artist Album Crawler (Version 1.0)
@@ -239,6 +268,9 @@ def clear_result_text():
 def insert_into_text_area(text_area, message):
     text_area.see(tk.END)  # 스크롤바를 맨 아래로 이동
     text_area.insert(tk.END, message + "\n")
+
+def show_url_entry_context_menu(event):
+    context_menu_url_entry.post(event.x_root, event.y_root)
 
 def show_context_menu(event):
     context_menu.post(event.x_root, event.y_root)
@@ -289,6 +321,8 @@ menubar.add_cascade(label="도움말(Help)", menu=help_menu)
 # 메뉴 설정
 root.config(menu=menubar)
 
+
+
 # 첫 번째 줄: 레이블, 입력 필드
 url_label = tk.Label(root, text="지니뮤직 아티스트 앨범 URL 입력:")
 url_label.grid(row=0, column=0, padx=10, pady=10)
@@ -315,7 +349,24 @@ scrollbar = Scrollbar(root, command=text_area.yview)
 scrollbar.grid(row=2, column=3, sticky='ns')
 text_area.config(yscrollcommand=scrollbar.set)
 
-# 오른쪽 클릭 컨텍스트 메뉴
+
+
+# url_entry의 오른쪽 클릭 컨텍스트 메뉴
+context_menu_url_entry = Menu(root, tearoff=0)
+context_menu_url_entry.add_command(label="모두 선택(Select All)", command=lambda: url_entry.select_range(0, 'end'))
+context_menu_url_entry.add_separator()  # 구분선 추가
+context_menu_url_entry.add_command(label="잘라내기(Cut)", command=lambda: url_entry.event_generate("<<Cut>>"))
+context_menu_url_entry.add_command(label="복사(Copy)", command=lambda: url_entry.event_generate("<<Copy>>"))
+context_menu_url_entry.add_command(label="붙여넣기(Paste)", command=lambda: url_entry.event_generate("<<Paste>>"))
+context_menu_url_entry.add_separator()
+context_menu_url_entry.add_command(label="삭제(Delete)", command=lambda: url_entry.delete(0, 'end'))
+
+# url_entry에 마우스 오른쪽 클릭 이벤트 바인딩
+url_entry.bind("<Button-3>", show_url_entry_context_menu)
+
+
+
+# text_area의 오른쪽 클릭 컨텍스트 메뉴
 context_menu = Menu(root, tearoff=0)
 context_menu.add_command(label="모두 선택(Select All)", command=select_all)
 context_menu.add_separator()  # 구분선 추가
@@ -325,7 +376,7 @@ context_menu.add_command(label="붙여넣기(Paste)", command=paste)
 context_menu.add_separator()
 context_menu.add_command(label="삭제(Delete)", command=delete)
 
-# 마우스 오른쪽 클릭 이벤트 바인딩
+# text_area에 마우스 오른쪽 클릭 이벤트 바인딩
 text_area.bind("<Button-3>", show_context_menu)
 
 
